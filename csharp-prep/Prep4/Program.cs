ff80aab72be00ce6a834869e6580229d21e8169e
mmmppc18 <and20147@byui.edu>
2025-01-21 22:39:26 -0700

    assignment 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -7,18 +8,38 @@ class Program
         List<int> numbers = new List<int>();
         
         int mathNumber = -1;
+        int count = -1;
 
         while (mathNumber != 0)
         {
             Console.Write("Enter a number or enter 0 to quit: ");
             mathNumber = int.Parse(Console.ReadLine());
 
-            if (mathNumber != 0)
+            if(mathNumber !=0)
             {
-
+            numbers.Add(mathNumber);
             }
-
+        }
+        int add = 0;
+        foreach (int number in numbers)
+        {
+            add += number;
         }
 
+        Console.WriteLine($"The sum is: {add}");
+
+        float average = ((float)add) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int high = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > high)
+            {
+                high = number;
+            }
+        }
+        Console.WriteLine ($"the max is: {high}");
     }
 }
