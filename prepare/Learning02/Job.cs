d9e7cbb08c2d15e9e6497f993dde27d75fe47d4f
mmmppc18 <and20147@byui.edu>
2025-01-22 22:04:14 -0700

    testing list code

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -7,8 +7,9 @@ public class Job
     public int _startYear;
     public int _endYear;
 
-    public void DisplayJobs()
+    public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
+
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Diagnostics.Contracts;
-using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
@@ -8,8 +7,13 @@ class Program
     {
         
         {
-            public string _name;
-            public double _jobs;   
-            List<Job> _jobs=new List<Job>(); 
+           Job job1 = new Job();
+           job1._jobTitle = "Cart Pusher";
+           job1._company = "Costco";
+           job1._startYear = 2000;
+           job1._endYear = 2010;
+           job1.Display();
+
         }
     }
+}
