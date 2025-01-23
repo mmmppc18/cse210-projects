a22aa68274547aa69adab2efce089d5cef404e65
mmmppc18 <and20147@byui.edu>
2025-01-22 21:42:31 -0700

    Built Job.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        public 
+        {
+            public string _company;
+            public string _jobTitle;
+            public int _startYear;
+            public int _endYear;
+
+            
+        }
+        public class Resume
+        {
+            public string _name;
+            public double _jobs;   
+            List<Job> _jobs=new List<Job>(); 
+        }
     }
-}
