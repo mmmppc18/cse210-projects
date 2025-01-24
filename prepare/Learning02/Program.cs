c58538c02afe68b98c0d4ed1238eb6770e3806e6
mmmppc18 <and20147@byui.edu>
2025-01-23 21:19:41 -0700

    Built Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,14 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+
         {
-           Job job1 = new Job();
-           job1._jobTitle = "Cart Pusher";
-           job1._company = "Costco";
-           job1._startYear = 2000;
-           job1._endYear = 2010;
-           job1.Display();
+            Job job1 = new Job();
+            job1._jobTitle = "Cart Pusher";
+            job1._company = "Costco";
+            job1._startYear = 2000;
+            job1._endYear = 2010;
+            job1.Display();
 
         }
     }
