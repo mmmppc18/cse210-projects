e7a7ff2ade2cb0ed8f472dcefb6992d4e4761a1f
mmmppc18 <and20147@byui.edu>
2026-02-28 17:29:01 -0700

    second assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+
+        int percentage = int.Parse(valueFromUser);
+
+        int a = 90;
+        int b = 80;
+        int c = 70;
+        int d = 60;
+
+        string letter = "";
+
+        if (percentage >= a)
+        {
+            letter = ("A");
+        }
+        else if (percentage >= b)
+        {
+            letter = ("B");
+        }
+        else if (percentage >= c)
+        {
+            letter = ("C");
+        }
+        else if (percentage >= d)
+        {
+            letter = ("D");
+        }
+        else
+        {
+            letter = ("F");
+        }
+            
+            Console.WriteLine($"Your grade is a '{letter}'."); 
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Don't give up! Try again!");
+        }
     }
 }
