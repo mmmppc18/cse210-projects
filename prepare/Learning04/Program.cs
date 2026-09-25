ed309ba5459009462aeb951d1d41b6fd6cbda1b9
mmmppc18 <and20147@byui.edu>
2026-09-24 20:55:12 -0600

    add the sum

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Create new list
+        List<int> numbers = new List<int>();
+        int newNumber = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.WriteLine("Enter a number: ");
+            newNumber = int.Parse(Console.ReadLine());
+            numbers.Add(newNumber);
+        }
+        while (newNumber != 0);
+
+        // add the sum of the number list
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.Write($"The sum is {sum}.");
+
     }
+
+
+
 }
