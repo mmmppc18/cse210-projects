6383e5d566297eb8cff2287d00dc8689beb04655
mmmppc18 <and20147@byui.edu>
2026-09-24 21:44:23 -0600

    adding functions

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        DisplayWelcome();
+
+        PromptUserName();
+        PromptUserNumber();
+        PromptUserBirthYear();
+        SquareNumber();
+        DisplayResult();
+
+
         Console.WriteLine("Hello Learning05 World!");
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    
+
+
+
 }
